4c77081e6448d2f76397049eb79ecfa0668286aa
marchalc <97352264+marchalc@users.noreply.github.com>
2023-03-31 07:51:09 +0200

    Add files via upload

---
+++ exo.js
@@ -0,0 +1,25 @@
+function BUT() { 
+		document.getElementById('forma').value="BUT réseaux et télécom";
+	    document.getElementById('nbeleve').value=50;
+	    document.getElementById('hsem').value = 35;
+	    document.getElementById('d').value = 3; 
+	    document.getElementById('stage').value = "oui"; 
+	}
+
+function licence() {  
+
+		document.getElementById('forma').value="Licence informatique";
+	    document.getElementById('nbeleve').value=160;
+	    document.getElementById('hsem').value = 25;
+	    document.getElementById('d').value = 3; 
+	    document.getElementById('stage').value = "non"; 
+	}
+
+function CPGE() {  
+
+		document.getElementById('forma').value="CPGE MP2i";
+	    document.getElementById('nbeleve').value=35;
+	    document.getElementById('hsem').value = 40;
+	    document.getElementById('d').value = 2; 
+	    document.getElementById('stage').value = "non"; 
+	}
